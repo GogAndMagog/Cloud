f52fad5db12f7f00f365bd1edad339ffba91796c
GogAndMagog <sergejsmel6@gmail.com>
2025-05-28 04:15:32 +0300

    feat(Security): add Redis integration

--- src/main/java/org/fizz_buzz/cloud/config/SecurityConfig.java
+++ src/main/java/org/fizz_buzz/cloud/config/SecurityConfig.java
@@ -9,8 +9,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
+@EnableRedisHttpSession
 public class SecurityConfig {
 
 
@@ -22,7 +24,9 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(form -> form.permitAll())
-                .logout(logout -> logout.permitAll());
+                .logout(logout -> logout.permitAll())
+//                .sessionManagement(session -> session.maximumSessions(2))
+        ;
         return http.build();
     }
 

b1341e584f81b5956ce1a528b00ee1a831b122c8
GogAndMagog <sergejsmel6@gmail.com>
2025-05-30 04:11:56 +0300

    fix(Security): fix session deserialization from json-format

--- src/main/java/org/fizz_buzz/cloud/config/SecurityConfig.java
+++ src/main/java/org/fizz_buzz/cloud/config/SecurityConfig.java
@@ -1,18 +1,14 @@
 package org.fizz_buzz.cloud.config;
 
-import org.springframework.boot.web.servlet.filter.OrderedFormContentFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-@EnableRedisHttpSession
 public class SecurityConfig {
 
 
@@ -25,7 +21,6 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(form -> form.permitAll())
                 .logout(logout -> logout.permitAll())
-//                .sessionManagement(session -> session.maximumSessions(2))
         ;
         return http.build();
     }
--- src/main/java/org/fizz_buzz/cloud/dto/CustomUserDetails.java
+++ src/main/java/org/fizz_buzz/cloud/dto/CustomUserDetails.java
@@ -1,32 +1,54 @@
 package org.fizz_buzz.cloud.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.fizz_buzz.cloud.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@Setter
 public class CustomUserDetails implements UserDetails {
 
-    private User user;
+    @JsonProperty("username")
+    private String name;
+
+    @JsonProperty("password")
+    private String pass;
+
+    @JsonProperty("authorities")
+    private List<? extends GrantedAuthority> authorities = new ArrayList<>();
+
 
     public CustomUserDetails(User user) {
-        this.user = user;
+        this.name = user.getName();
+        this.pass = user.getPassword();
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return user.getPassword();
+        return pass;
     }
 
     @Override
     public String getUsername() {
-        return user.getName();
+        return name;
     }
 }

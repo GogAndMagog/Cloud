dfb6443da00de2b5df82f893d58caeedcf5d71de
GogAndMagog <sergejsmel6@gmail.com>
2025-07-15 22:59:23 +0300

    Revert "fix(AuthController): reset to Controller-level sign-out for API unification"
    This reverts commit b776ec182a41d1c22e1e4193f4602a74b7a84d62.

--- src/main/java/org/fizz_buzz/cloud/config/SecurityConfig.java
+++ src/main/java/org/fizz_buzz/cloud/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package org.fizz_buzz.cloud.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,12 +9,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
 
 @Configuration
 public class SecurityConfig {
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,9 +24,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(form -> form.disable())
-                .logout(logout -> logout
-                        .logoutUrl("/api/v1/auth/sign-out")
-                        .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT)))
+                .logout(logout -> logout.disable())
         ;
         return http.build();
     }
@@ -44,9 +41,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityContextRepository securityContextRepository() {
+    public SecurityContextRepository securityContextRepository(){
 
         return new HttpSessionSecurityContextRepository();
     }
-
 }
--- src/main/java/org/fizz_buzz/cloud/controller/AuthController.java
+++ src/main/java/org/fizz_buzz/cloud/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.fizz_buzz.cloud.dto.response.UserResponseDTO;
 import org.fizz_buzz.cloud.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,16 @@ public class AuthController {
         var response = authService.signIn(request, httpServletRequest, httpServletResponse);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/sign-out")
+    public ResponseEntity<Void> signOut(Authentication authentication,
+                                        HttpServletRequest request,
+                                        HttpServletResponse response) {
+
+        this.logoutHandler.logout(request, response, authentication);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping

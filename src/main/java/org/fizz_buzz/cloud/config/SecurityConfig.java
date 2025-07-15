b776ec182a41d1c22e1e4193f4602a74b7a84d62
GogAndMagog <sergejsmel6@gmail.com>
2025-07-15 22:50:29 +0300

    fix(AuthController): reset to Controller-level sign-out for API unification

--- src/main/java/org/fizz_buzz/cloud/config/SecurityConfig.java
+++ src/main/java/org/fizz_buzz/cloud/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.fizz_buzz.cloud.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -9,12 +10,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
 
 @Configuration
 public class SecurityConfig {
-
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,7 +25,9 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(form -> form.disable())
-                .logout(logout -> logout.disable())
+                .logout(logout -> logout
+                        .logoutUrl("/api/v1/auth/sign-out")
+                        .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT)))
         ;
         return http.build();
     }
@@ -41,8 +44,9 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityContextRepository securityContextRepository(){
+    public SecurityContextRepository securityContextRepository() {
 
         return new HttpSessionSecurityContextRepository();
     }
+
 }

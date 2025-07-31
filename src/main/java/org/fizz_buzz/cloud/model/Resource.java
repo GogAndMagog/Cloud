1495315b3507cc4dbb28885acffe9bdbbf7ec0c3
GogAndMagog <sergejsmel6@gmail.com>
2025-08-01 02:27:37 +0300

    fix(Resource): close dataStream at object destruction

--- src/main/java/org/fizz_buzz/cloud/model/Resource.java
+++ src/main/java/org/fizz_buzz/cloud/model/Resource.java
@@ -1,6 +1,20 @@
 package org.fizz_buzz.cloud.model;
 
+import lombok.SneakyThrows;
+
 import java.io.InputStream;
+import java.lang.ref.Cleaner;
+import java.util.Objects;
 
 public record Resource(String path, InputStream dataStream, long size) {
+
+    private static final Cleaner cleaner = Cleaner.create();
+
+    @SneakyThrows
+    public Resource {
+
+        Objects.requireNonNull(dataStream);
+
+        cleaner.register(this, this.dataStream()::close);
+    }
 }

509296b941bb49700e0c8211ed989094a7ecf52d
GogAndMagog <sergejsmel6@gmail.com>
2025-08-01 04:29:26 +0300

    fix(Resource): add wrapper for checked exceptions in Runnable

--- src/main/java/org/fizz_buzz/cloud/model/Resource.java
+++ src/main/java/org/fizz_buzz/cloud/model/Resource.java
@@ -1,20 +1,34 @@
 package org.fizz_buzz.cloud.model;
 
-import lombok.SneakyThrows;
-
 import java.io.InputStream;
 import java.lang.ref.Cleaner;
 import java.util.Objects;
 
 public record Resource(String path, InputStream dataStream, long size) {
 
-//    private static final Cleaner cleaner = Cleaner.create();
+    private static final Cleaner cleaner = Cleaner.create();
 
-//    @SneakyThrows
-//    public Resource {
-//
-//        Objects.requireNonNull(dataStream);
-//
-//        cleaner.register(this, this.dataStream()::close);
-//    }
+    @FunctionalInterface
+    private interface ThrowingRunnable {
+
+        void run() throws Exception;
+    }
+
+    private static void wrap(ThrowingRunnable throwingConsumer) {
+
+        try {
+
+            throwingConsumer.run();
+        } catch (Exception e) {
+
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Resource {
+
+        Objects.requireNonNull(dataStream);
+
+        cleaner.register(this, () -> wrap(this.dataStream()::close));
+    }
 }
